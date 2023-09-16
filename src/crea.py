0976f1238ee95366a1baa9998c39afbc796749aa
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-09-16 16:51:10 -0400

    Setting up loop for columns

--- src/crea.py
+++ src/crea.py
@@ -12,13 +12,22 @@ df_crea = pd.read_excel(
     "../research/data/crea/crea-all-time.xlsx",
 )
 
-## Part 1: Setting up Columns
+## Part 1: Pulling Information about Columns
 measures = df_crea.iloc[0,:][df_crea.iloc[0, :].notna()].to_list()[1:]
-measures_idx = np.where(df_crea.iloc[0, :].notna())[0][1:]
+measures_idx = np.where(df_crea.iloc[0, :].notna())[0][1:] - 1
 regions = df_crea.iloc[1,:][df_crea.iloc[1, :].notna()].to_list()[1:]
-regions_idx = np.where(df_crea.iloc[1, :].notna())[0][1:]
+regions_idx = np.where(df_crea.iloc[1, :].notna())[0][1:] - 1
 adjustments = df_crea.iloc[3,:][df_crea.iloc[3, :].notna()].to_list()[1:]
-adjustments_idx = np.where(df_crea.iloc[3, :].notna())[0][1:]
+adjustments_idx = np.where(df_crea.iloc[3, :].notna())[0][1:] - 1
 
-for measure, measure_idx in zip(measures, measures_idx):
-    print(f"{measure}: {measure_idx}")
+## Part 2: Setting up Columns
+cols = [""] * df_crea.shape[1]
+
+for idx in range(df_crea.shape[1]):
+    pass
+
+for i, measure in enumerate(measures):
+    start_idx = measures_idx[i]
+    end_idx = measures_idx[i+1] if i+1 < len(measures_idx) else len(cols)
+    import ipdb; ipdb.set_trace(context = 10) 
+    print(f"{start_idx}:{end_idx}")
